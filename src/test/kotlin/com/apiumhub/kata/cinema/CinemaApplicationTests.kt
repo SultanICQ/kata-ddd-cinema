9c7961ce238fb607d75d57c8facf1e86dde73035
David Garcia Latorre <sultanicq@gmail.com>
2025-03-18 18:30:31 +0100

    Initial Commit

---
+++ src/test/kotlin/com/apiumhub/kata/cinema/CinemaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.apiumhub.kata.cinema
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
